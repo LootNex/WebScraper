63423888bcaef2bdd1df2e87f779f20d53728ae1
LootNex <nikitas54321@mail.ru>
2025-05-01 11:41:42 +0300

    add auth and tg bot

--- init.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS items(
-    Id UUID PRIMARY KEY,
-    user_id VARCHAR(100),
-    link VARCHAR(100),
-    product_name VARCHAR(100),
-    start_price REAL,
-    current_price REAL,
-    creation_date TIMESTAMP
-);
